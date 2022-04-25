ea3881b2347a7033a3fdd6ff304787b01369e080
Shaked Sibony <shaked.shaked@gmail.com>
2022-04-25 23:31:33 +0300

    Fixed globus logo and navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import logo from '../../images/LogoWhite.png';
+import logo from '../../images/GlobusCarsLogoBlueCrop.png';
 
 const Navbar = ({ token }) =>
 {
