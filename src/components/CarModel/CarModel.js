f0ec93b76961ad18ca8968748f887edc61fb10b3
Shaked Sibony <shaked@shaked.com>
2022-04-25 12:14:34 +0300

    Car Model design fixed to look better.

--- src/components/CarModel/CarModel.js
+++ src/components/CarModel/CarModel.js
@@ -4,19 +4,31 @@ const CarModel = ({ model }) =>
 {
     return (
         <div className="CarModel">
-            <h2> {model.brand} - {model.modelName}</h2>
-            <img alt="car" src={model.img} />
-            <div className="flex-row">
-                <h3>שנת: {model.year} </h3>
-                <h3>קילומארז: {model.year} </h3>
-                <h3>סניף: {model.year} </h3>
-            </div>
-            <h3>הערות: {model.review}</h3>
 
+            <img alt="car" id="Car" src={model.img} />
             <div className="flex-row">
-                <h3>מחיר ליום: {model.pricePerDay}₪ </h3>
-                <h3>מחיר לשבוע: {model.pricePerDay}₪ </h3>
+                <img id="Logo" alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/9/9f/Opel-Logo_2017.svg" />
+                <div className="bold">
+                    <p>{model.brand}</p>
+                    <p>{model.modelName}</p>
+                </div>
             </div>
+            <line />
+            <tr>
+                <td>Model year:</td>
+                <td>{model.year}</td>
+            </tr>
+            <p>{model.review}</p>
+            <line />
+            <tr>
+                <td>Price Per Day:</td>
+                <td>{model.pricePerDay}₪</td>
+            </tr>
+            <tr>
+                <td>Price Per Week:</td>
+                <td>{7 * model.pricePerDay}₪</td>
+            </tr>
+            <button>הזמן</button>
         </div>
     );
 };
