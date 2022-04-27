262c0f4c1a71fbe066725f2030a47f9787e418ce
Shaked Sibony <shaked.shaked@gmail.com>
2022-04-27 20:35:59 +0300

    ButtonEffects class added

--- src/components/CarSegment/CarSegment.js
+++ src/components/CarSegment/CarSegment.js
@@ -5,7 +5,7 @@ const CarSegment = ({ type, imgsrc, review }) =>
 {
   return (
     <Link to={type}>
-      <div className="CarSegment Grow LightCard">
+      <div className="CarSegment ButtonEffects LightCard">
         <img alt="car" src={imgsrc} />
         <li>
           <h2>{type}</h2>
